bf1094c6c18e6a1be292dbcdcdd517410694c338
Sndrahel <sandra.helander@hotmail.com>
2021-10-05 20:07:52 +0000

    Chore(core): Copied and pasted content from old project base.css and fixed som urls

--- happy_feets/urls.py
+++ happy_feets/urls.py
@@ -22,6 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('about/', include('about.urls')),
-    path('treatments/', include('treatments.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
